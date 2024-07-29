77350162c8aabb781d5899dfe46f831c41fb8741
you55efmansour <mlmleg2017@gmail.com>
2024-07-29 19:24:14 +0200

    update

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ listBox.addEventListener("click", () => {
     listBox.children[0].innerHTML = "x";
   }
 });
+
+
+// Get the elements
+const thumbnail = document.getElementsByClassName('pop-img');
+const popup = document.getElementById('popup');
+const popupImage = document.getElementById('popup-image');
+const close = document.querySelector('.close');
+console.log(thumbnail);
+
+// When the user clicks on the thumbnail, open the popup
+[...thumbnail].forEach(element => {
+  element.onclick = function() {
+    popup.style.display = 'block';
+    popupImage.src = this.src; // Set the popup image source to the clicked image source
+};
+});
+
+// When the user clicks on the close button, close the popup
+close.onclick = function() {
+    popup.style.display = 'none';
+};
+
+// When the user clicks anywhere outside the popup, close the popup
+window.onclick = function(event) {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+};
